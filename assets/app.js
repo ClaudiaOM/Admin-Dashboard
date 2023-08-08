5d6523065844ea50e99d09eb645fbb5a91995b73
Claudia Olavarrieta <claudiaomtnez@gmail.com>
2023-08-08 15:29:35 -0400

    Project section layout

---
+++ assets/app.js
@@ -0,0 +1,28 @@
+window.onload = function() {
+    const projectContainer = document.querySelector('#project-container');
+
+    for (let i = 0; i < 6; i++) {
+        const projectCard = document.createElement('div');
+        projectCard.classList.add('project-card');
+
+        const colorColumn = document.createElement('div');
+        colorColumn.classList.add('color-column');
+        projectCard.appendChild(colorColumn);
+
+        const title = document.createElement('h1');
+        title.textContent = 'Project Title';
+        projectCard.appendChild(title);
+
+        const descriptionContainer = document.createElement('div');
+        const description = document.createElement('p');
+        description.textContent = 'Project Description';
+        descriptionContainer.appendChild(description);
+        projectCard.appendChild(descriptionContainer);
+
+        const projectIcons = document.createElement('div');
+        projectIcons.classList.add('project-icons');
+        projectCard.appendChild(projectIcons);
+
+        projectContainer.appendChild(projectCard);
+    }
+}
